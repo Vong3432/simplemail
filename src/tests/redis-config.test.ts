1669567e918ad8f3744b024d4391a266b2f37199
Vong3432 <vongnyuksoon2000@gmail.com>
2021-12-22 23:28:25 +0800

    chore: Deleted unused code in test

--- src/tests/redis-config.test.ts
+++ src/tests/redis-config.test.ts
@@ -19,18 +19,5 @@ describe('Redis configuration', () => {
         expect(host).toEqual("ec122-123-123-123-123.compute-123.amazonaws.com")
         expect(password).toEqual("aishduh1ui23h1ui23h")
         expect(port).toEqual("17621")
-        // const res = await request(server)
-        //     .post('/send-email')
-        //     .send({
-        //         "email": config.fromEmail,
-        //         "from": config.fromEmail,
-        //         "subject": "Send email",
-        //         "text": "Testing",
-        //         "method": "POST",
-        //         "html": "<h3>Test mail endpoint</h3>",
-        //         "smtp_pass": config.smtpAppPass,
-        //         "smtp_user": config.fromEmail,
-        //     });
-        // expect(res.status).toEqual(201)
     })
 })
